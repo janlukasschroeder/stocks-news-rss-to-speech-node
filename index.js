345a9f3c151a8f984671cd6774af4337cc46ee40
janlukasschroeder <janlukasschroeder@protonmail.com>
2018-09-01 21:05:06 +1000

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const say = require('say');
+const Parser = require('rss-parser');
+const parser = new Parser();
+const moment = require('moment');
+
+const url = 'http://apps.shareholder.com/rss/rss.aspx?channels=6864&companyid=ABEA-45YXOQ&sh_auth=6377614456%2E0%2E0%2E43345%2E374806b9f234407f20f2b26e92154f03';
+// Use default system voice and speed
+
+parser.parseURL(url).then(feed => {
+
+  talk(feed.items);
+
+});
+
+const talk = (arr) => {
+  if (arr instanceof Array) {
+    const item = arr.shift(); // get and remove first element of array
+    const text = moment(item.pubDate).format('dddd') + ': ' + item.title;
+    say.speak(text, 'Alex', 1.5, (err) => {
+      return arr.length > 0 ? talk(arr) : -1;
+    });
+  }
+};
+
